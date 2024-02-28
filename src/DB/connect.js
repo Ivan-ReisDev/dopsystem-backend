25256b39c13f4e97f7e0c5a7fc9bef07c9ebaf79
Ivan Reis <ivanreisdev@gmail.com>
2024-02-28 20:12:11 -0300

    up vercel

--- src/DB/connect.js
+++ src/DB/connect.js
@@ -8,7 +8,7 @@ async function main(){
         const dbPassword = process.env.DB_PASS
 
          mongoose.set("strictQuery", true)
-        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.dvmxyl9.mongodb.net/?retryWrites=true&w=majority`);
+        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.dvmxyl9.mongodb.net/?retryWrites=true&w=majority`);
         console.log("Banco Conectado", mongoose.connection.host);
     } catch (error) {
         console.log(`error: ${error}`);
