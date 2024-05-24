67a6ececd6d6862156c9e23a2bfdbae245587efe
Ivan Reis <ivanreisdev@gmail.com>
2024-05-23 21:02:02 -0300

    feat: Incluindo novos recusos

--- src/DB/connect.js
+++ src/DB/connect.js
@@ -8,7 +8,7 @@ async function main(){
         // const dbPassword = process.env.DB_PASS
 
          mongoose.set("strictQuery", true)
-        await mongoose.connect(process.env.DB_CONNECT);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.dvmxyl9.mongodb.net/?retryWrites=true&w=majority`);
         console.log("Banco Conectado", mongoose.connection.host);
     } catch (error) {
         console.log(`error: ${error}`);
