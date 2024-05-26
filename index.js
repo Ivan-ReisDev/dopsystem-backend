9a70e3bdd8aa4d26ce01baf38cbc314ba8a22b53
Ivan Reis <ivanreisdev@gmail.com>
2024-05-25 22:16:29 -0300

    Mudança de porta

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-const port =  process.env.PORT_APP || 3000;
+const port = process.env.PORT_APP || 3001;
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 app.use(express.json());
 
-const connectdb = require('./src/DB/connect.js')
+const connectdb = require('./src/DB/connect.js');
 connectdb();
-const routes = require('./src/routes/router.js')
-app.use('/api', routes)
+const routes = require('./src/routes/router.js');
+app.use('/api', routes);
 
-app.listen(3000, function(){
-    console.log(`Server online na porta ${port}, acesse: http://localhost:${port}/` );
-})
+app.listen(port, function() {
+    console.log(`Server online na porta ${port}, acesse: http://localhost:${port}/`);
+});
