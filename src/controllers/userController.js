84cb7221c29f90e033727ecd3757c74a6eeb6e53
Ivan Reis <ivanreisdev@gmail.com>
2024-06-13 20:58:42 -0300

    Novo recurto no RH

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -352,7 +352,7 @@ const serviceControllerUser = {
     try {
       const nickname = req.query.nickname;
       const typeRequeriment = req.query.typeRequeriment;
-      const users = await User.find().sort({ nickname: 1 }).select("-password");
+      const users = await User.findOne().sort({ nickname: 1 }).select("-password");
       const resUser = nickname
         ? users.filter(user => user.nickname.includes(nickname))
         : users;
