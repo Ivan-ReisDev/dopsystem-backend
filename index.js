3c15e6229c97254bfe2365d13ed06d0947417e84
Ivan Reis <ivanreisdev@gmail.com>
2024-06-28 23:05:43 -0300

    :lock: feat: Melhoria na autenticação/segurança usando token httponly

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const app = express();
-
-// const port = process.env.PORT_APP || 3000;
-
-// app.use(bodyParser.json({ limit: '10mb' }));
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-
-// app.use(cors());
-// app.use(express.json());
-
-// const connectdb = require('./src/DB/connect.js');
-// connectdb();
-// const routes = require('./src/routes/router.js');
-// app.use('/api', routes);
-
-// app.listen(port, function() {
-//     console.log(`Server online na porta ${port}, acesse: http://localhost:${port}/`);
-// });
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require("fs");
-const https = require("https")
-const router = require("./src/routes/service")
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const https = require('https');
+const router = require('./src/routes/service');
 
 // Porta para servidor HTTP
 const port = process.env.PORT_APP || 3000;
@@ -37,7 +16,14 @@ const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser()); // Adiciona suporte para parsing de cookies
+
+// Configurações de CORS para permitir cookies
+app.use(cors({
+  origin: 'https://policiadop.com.br', // Altere para o domínio da sua aplicação cliente
+  credentials: true
+}));
+
 const connectdb = require('./src/DB/connect.js');
 connectdb();
 app.use('/api', router);
@@ -48,13 +34,13 @@ app.listen(port, () => {
 });
 
 // Verifica se os certificados SSL estão disponíveis antes de inicializar o servidor HTTPS
-if (fs.existsSync("src/SSL/code.crt") && fs.existsSync("src/SSL/code.key")) {
+if (fs.existsSync('src/SSL/code.crt') && fs.existsSync('src/SSL/code.key')) {
   https.createServer({
-    cert: fs.readFileSync("src/SSL/code.crt"),
-    key: fs.readFileSync("src/SSL/code.key")
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
   }, app).listen(httpsPort, () => {
     console.log(`Servidor HTTPS rodando na porta ${httpsPort}`);
   });
 } else {
-  console.log("Certificados SSL não encontrados. Servidor HTTPS não será iniciado.");
+  console.log('Certificados SSL não encontrados. Servidor HTTPS não será iniciado.');
 }
--- src/Middleware/authGuard.js
+++ src/Middleware/authGuard.js
@@ -1,41 +1,28 @@
 const { User } = require('../Models/useModel');
-const jwt = require("jsonwebtoken");
-
+const jwt = require('jsonwebtoken');
 // Middleware de autorização
 const authGuard = (requiredRoles) => {
     return async (req, res, next) => {
         try {
-            // Obter o token do cabeçalho de autorização
-            const authHeader = req.headers["authorization"];
-            if (!authHeader) {
-                return res.status(401).json({ errors: ["Acesso negado!"] });
-            }
-
-            const token = authHeader.split(" ")[1];
+            // Obter o token do cookie HTTP-only
+            const token = req.cookies.token;
+            console.log(token)
             if (!token) {
-                return res.status(401).json({ errors: ["Acesso negado!"] });
-            }
-
-            // Log do token para depuração
-            console.log("Token recebido:", token);
-
-            // Verificar se o token tem o formato correto
-            if (token.split('.').length !== 3) {
-                return res.status(400).json({ errors: ["Token malformado."] });
+                return res.status(401).json({ errors: ['Acesso negado!'] });
             }
 
             // Verificar o token usando a chave secreta
             const verified = jwt.verify(token, process.env.JWT_SECRET);
 
             // Adicionar informações do usuário autenticado à requisição (req)
-            req.user = await User.findById(verified.id).select("-password");
+            req.user = await User.findById(verified.id).select('-password');
             if (!req.user) {
-                return res.status(404).json({ errors: ["Usuário não encontrado."] });
+                return res.status(404).json({ errors: ['Usuário não encontrado.'] });
             }
 
             // Verificar se o usuário tem uma das permissões necessárias
             if (!requiredRoles.includes(req.user.userType)) {
-                return res.status(403).json({ errors: ["Permissão insuficiente."] });
+                return res.status(403).json({ errors: ['Permissão insuficiente.'] });
             }
 
             // Chamar o próximo middleware
@@ -43,9 +30,9 @@ const authGuard = (requiredRoles) => {
         } catch (err) {
             console.error(err);
             // Responder com erro em caso de token inválido ou qualquer outro erro
-            res.status(401).json({ errors: ["Token inválido."] });
+            res.status(401).json({ errors: ['Token inválido.'] });
         }
-    }
+    };
 };
 
 module.exports = authGuard;
