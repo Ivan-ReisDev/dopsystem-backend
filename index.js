a524fdeea4672bd15fcf425cd8d51820b213af92
Ivan Reis <ivanreisdev@gmail.com>
2024-07-03 18:12:34 -0300

    Corrigindo erro

--- index.js
+++ index.js
@@ -1,49 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
+const cookieParser = require('cookie-parser');
+const router = require("./src/routes/router.js");
 
-const router = require("./src/routes/service");
+
+const port = process.env.PORT_APP || 3000;
 
 const app = express();
-const port = process.env.PORT_APP || 3000;
-const httpsPort = 443;
 
-// Middleware para parsear JSON e URL-encoded
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+// Configuração do CORS
 
-// Middleware para permitir requisições CORS
+
 app.use(cors());
 
-// Middleware para parsear o body como JSON
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
-// Conexão com o banco de dados MongoDB
-const connectdb = require('./src/DB/connect');
+const connectdb = require('./src/DB/connect.js');
 connectdb();
 
-// Rotas
 app.use('/api', router);
 
-// Servidor HTTP
-const httpServer = http.createServer(app);
-httpServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor HTTP online na porta ${port}, acesse: http://localhost:${port}/`);
 });
-
-// Servidor HTTPS
-if (fs.existsSync("src/SSL/code.crt") && fs.existsSync("src/SSL/code.key")) {
-  const httpsOptions = {
-    cert: fs.readFileSync("src/SSL/code.crt"),
-    key: fs.readFileSync("src/SSL/code.key")
-  };
-  const httpsServer = https.createServer(httpsOptions, app);
-  httpsServer.listen(httpsPort, () => {
-    console.log(`Servidor HTTPS rodando na porta ${httpsPort}`);
-  });
-} else {
-  console.log("Certificados SSL não encontrados. Servidor HTTPS não será iniciado.");
-}
--- src/DB/connect.js
+++ src/DB/connect.js
@@ -1,21 +1,48 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-const dbName = process.env.DB_NAME;
-const dbHost = process.env.DB_HOST;
+require('dotenv').config()
 
 async function main() {
     try {
+        const dbUser = process.env.DB_USER;
+        const dbPassword = process.env.DB_PASSK;
+        const dbName = process.env.DB_NAMEK;
+        const dbHost = process.env.DB_HOST;
+
         mongoose.set("strictQuery", true);
 
-        await mongoose.connect(`mongodb://${dbUser}:${encodeURIComponent(dbPassword)}@${dbHost}/${dbName}?retryWrites=true&w=majority`);
+        const uri = `mongodb://${dbUser}:${encodeURIComponent(dbPassword)}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
 
-        console.log("Banco Conectado:", mongoose.connection.host);
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Banco Conectado", mongoose.connection.host);
     } catch (error) {
-        console.error("Erro ao conectar ao banco de dados:", error);
+        console.log(`error: ${error}`);
     }
 }
 
 module.exports = main;
+
+
+
+// const mongoose = require("mongoose");
+// require('dotenv').config()
+
+// async function main(){
+// //TESTE
+//     try {
+//         // const dbUser = process.env.DB_USER
+//         // const dbPassword = process.env.DB_PASS
+
+//          mongoose.set("strictQuery", true)
+//         await mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.dvmxyl9.mongodb.net/?retryWrites=true&w=majority`);
+//         console.log("Banco Conectado", mongoose.connection.host);
+//     } catch (error) {
+//         console.log(`error: ${error}`);
+//     }
+// }
+
+
+// module.exports = main
