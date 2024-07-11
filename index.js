fb917ebb6f341939e82249bbfed19a5e6fa4183b
Ivan Reis <ivanreisdev@gmail.com>
2024-07-11 11:09:02 -0300

    :bug: fix: correção de bug ao criar um novo requerimento de contrato e adição da opção deletar usuário

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require("./src/routes/router.js");
 
-
 const port = process.env.PORT_APP || 3000;
 
 const app = express();
 
 // Configuração do CORS
-app.use(cors());
+const corsOptions = {
+  origin: 'https://policiadop.com.br', // Substitua com a URL da sua aplicação frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
-
 
 const connectdb = require('./src/DB/connect.js');
 connectdb();
